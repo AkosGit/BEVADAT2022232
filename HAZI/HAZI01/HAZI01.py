dad77d563c26b6a56bb5bfe001c8761c402983b9
Open Sauce Projects <46137184+AkosGit@users.noreply.github.com>
2023-03-02 15:23:45 +0100

    commented out test cses

--- HAZI/HAZI01/HAZI01.py
+++ HAZI/HAZI01/HAZI01.py
@@ -12,7 +12,7 @@ def subset(input_list,start_index,end_index):
         if(i >= start_index and i<= end_index):
             out.append(input_list[i])
     return out
-subset([1,2,3,4,5], 1,3)
+#subset([1,2,3,4,5], 1,3)
 
 # %%
 #Create a function that returns every nth element of a list.
@@ -27,7 +27,7 @@ def every_nth(input_list,step_size):
         if (i+1)%step_size==0:
             out.append(input_list[i])
     return out
-every_nth([0,1,2,3,4],2)
+#every_nth([0,1,2,3,4],2)
 
 # %%
 #Create a function that can decide whether a list contains unique values or not
@@ -44,7 +44,7 @@ def unique(input_list):
         else:
             return False
     return True
-unique([1,0,1])
+#unique([1,0,1])
 
 # %%
 #Create a function that can flatten a nested list ([[..],[..],..])
@@ -59,7 +59,7 @@ def flatten(input_list):
         for e in l:
             out.append(e)
     return out
-flatten([[1,2],[3,4],[5,6]])
+#flatten([[1,2],[3,4],[5,6]])
 
 # %%
 #Create a function that concatenates n lists
@@ -74,7 +74,7 @@ def merge_lists(*args):
     for l in args:
         out.extend(l)
     return out
-merge_lists([1,2],[3,4],[5,6])
+#merge_lists([1,2],[3,4],[5,6])
 
 # %%
 #Create a function that can reverse a list of tuples
@@ -91,7 +91,7 @@ def reverse_tuples(input_list):
             l.append(input_list[i][j])
         input_list[i]=tuple(l)
     return input_list
-reverse_tuples([(1,2), (3,4)])
+#reverse_tuples([(1,2), (3,4)])
 
 # %%
 #Create a function that removes duplicates from a list
@@ -106,7 +106,7 @@ def remove_tuplicates(input_list):
         if e not in out:
             out.append(e)
     return out
-remove_tuplicates([1,2,2,3,4,4]) 
+#remove_tuplicates([1,2,2,3,4,4]) 
 
 # %%
 #Create a function that transposes a nested list (matrix)
@@ -123,11 +123,11 @@ def transpose(input_list):
                 out.append([])
             out[j].append(input_list[i][j])  
     return out
-transpose([
-    [1,2],
-    [3,4],
-    [5,6]
-])
+#transpose([
+    #[1,2],
+    #[3,4],
+    #[5,6]
+#])
 
 
 # %%
@@ -153,7 +153,7 @@ def split_into_chunks(input_list,chunk_size):
     if len(n)!=0:
         out.append(n)
     return out
-split_into_chunks([[1],[2,3],[4,5,6]], 5)
+#split_into_chunks([[1],[2,3],[4,5,6]], 5)
 
 # %%
 #Create a function that can merge n dictionaries
@@ -168,7 +168,7 @@ def merge_dicts(*dict):
         for key in dic.keys():
             out[key]=dic[key]
     return out
-merge_dicts({1:"one", 2: "two"}, {3:"three",4:"four",5:"five"})
+#merge_dicts({1:"one", 2: "two"}, {3:"three",4:"four",5:"five"})
 
 # %%
 #Create a function that receives a list of integers and sort them by parity
@@ -188,7 +188,7 @@ def by_parity(input_list):
         else:
             out["odd"].append(e)
     return out
-by_parity([1,2,3,4,5,6])
+#by_parity([1,2,3,4,5,6])
 
 # %%
 #Create a function that receives a dictionary like this: {"some_key":[1,2,3,4],"another_key":[1,2,3,4],....}
@@ -206,7 +206,7 @@ def mean_key_value(input_dict):
             m+=e
         input_dict[key]=int(m/len(input_dict[key]))
     return input_dict
-mean_key_value({"somekey":[1,2,3],"key": [5,5]})
+#mean_key_value({"somekey":[1,2,3],"key": [5,5]})
 
 # %%
 #If all the functions are created convert this notebook into a .py file and push to your repo
