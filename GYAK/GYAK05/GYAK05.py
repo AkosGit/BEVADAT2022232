0d6b67a145076c13bc5112d32770c8f0116ebbd3
Open Sauce Projects <46137184+AkosGit@users.noreply.github.com>
2023-03-30 21:00:40 +0200

    GYAK05 finished

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,52 +1,46 @@
-# %%
 import numpy as np
 from scipy.stats import mode
 from typing import Tuple
 from sklearn.metrics import confusion_matrix
-class KNNClassifier:
+class KNNClassifier:   
+    @property
+    def k_neighbors(self):
+        return self.k
     def __init__(self, k :int,test_split_ratio :float) -> None:
-        pass 
-    def load_csv(lul)-> tuple[np.ndarray,np.ndarray]:
+            self.test_split_ratio=test_split_ratio
+            self.k=k
+    @staticmethod
+    def load_csv(lul):
         np.random.seed(42)
         dataset = np.genfromtxt(lul,delimiter=',')
         print(dataset.shape)
         np.random.shuffle(dataset)
         x,y = dataset[:,:-1],dataset[:,-1] # ???
         return x,y
-    x,y = load_csv("iris.csv")
 
-    def train_test_split(features:np.ndarray,lables:np.ndarray,test_split_ratio:float):
+    def train_test_split(self,features:np.ndarray,lables:np.ndarray,test_split_ratio:float):
         test_size = (len(features * test_split_ratio))
         train_size = len(features)- test_size
         assert len(features) == test_size + train_size, "Size mismatch"
+        self.x_train,self.y_train=features[:train_size,:],lables[:train_size]
+        self.x_test,self.y_test=features[train_size:,:],lables[train_size:]
 
-        x_train,y_train=features[:train_size,:],lables[:train_size]
-        x_test,y_test=features[train_size:,:],lables[train_size:]
-        return(x_train,y_train,x_test,y_test)
-    x_train,y_train,x_test,y_test = train_test_split(x,y,0.2)
-
-    def euclidean(points,element_of_x):
-        return np.sqrt(np.sum((points-element_of_x)**2,axis=0))
-
-
-
-    def predict(x_train,y_train,x_test,y_test,k):
+    def euclidean(self,element_of_x):
+        return np.sqrt(np.sum((self.x_train-element_of_x)**2,axis=0))
+    
+    def predict(self):
         labels = []
-        for x_test_element in x_test:
+        for x_test_element in self.x_test:
             #táv
-            distances = euclidean(x_train,x_test_element)
-            distances = np.array(sorted(zip(distances,y_train)))
+            distances = self.euclidean(self.x_train,x_test_element)
+            distances = np.array(sorted(zip(distances,self.y_train)))
             #leggyakoribb label
-            labels_pred = mode(distances[:k,1],keepdims = False).mode
+            labels_pred = mode(distances[:self.k,1],keepdims = False).mode
             labels_pred.append(labels_pred)
-        return np.array(labels_pred,dtype=np.int64)
-    predict(x_train,y_train,x_test,y_test,3)
+        self.y_preds= np.array(labels_pred,dtype=np.int64)
 
-
-
-    def accuracy(y_test,y_preds):
-        true_positive = (y_test == y_preds).sum()
-        return true_positive / len(y_test) *100
-    accuracy(y_test,y_preds)
-
-
+    def accuracy(self,):
+        true_positive = (self.y_test == self.y_preds).sum()
+        return true_positive / len(self.y_test) *100
+    def confusion_matrix(self):
+         return confusion_matrix(self.y_test,self.y_preds)
