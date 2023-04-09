db3e6bd8817ef6634c95afd4dba3e463a20cb6c7
Open Sauce Projects <46137184+AkosGit@users.noreply.github.com>
2023-04-09 23:33:45 +0200

    added grid search

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -1,29 +1,27 @@
-# %%
 import numpy as np
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
-
-from src.Node import Node
-#from src.DecisionTreeClassifier import DecisionTreeClassifier
 
 class Node():
     def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
         ''' constructor ''' 
         
         # for decision node
+        # the condition si defined by feature_index and 
+        # self.feature_index is an integer representing the index of the feature that the node splits on.
         self.feature_index = feature_index
+        # self.threshold is the value of the feature that the node splits on.
         self.threshold = threshold
         self.left = left
         self.right = right
+        # self.info_gain is the information gain that was achieved by splitting on the feature and threshold represented by the node
         self.info_gain = info_gain 
         
         # for leaf node
+
         self.value = value
-
-
 
 class DecisionTreeClassifier():
     def __init__(self, min_samples_split=2, max_depth=2):
@@ -37,32 +35,40 @@ class DecisionTreeClassifier():
         
         X, Y = dataset[:,:-1], dataset[:,-1]
         num_samples, num_features = np.shape(X)
-        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth: # havent reached max depth in tree and minimum spale size is achived
             best_split = self.get_best_split(dataset, num_samples, num_features)
-            if best_split["info_gain"]>0: 
+            if best_split["info_gain"]>0: # if info gained 0, it means the node is pure: it contains one class of items
                 left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
                 right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
                 return Node(best_split["feature_index"], best_split["threshold"],
                             left_subtree, right_subtree, best_split["info_gain"])
         
-
+        # leaf node is the last node in the tree
         leaf_value = self.calculate_leaf_value(Y)
         return Node(value=leaf_value)
     
     def get_best_split(self, dataset, num_samples, num_features):
         ''' function to find the best split '''
         
+            # dictionary to store the best split
         best_split = {}
         max_info_gain = -float("inf")
         
+        # loop over all the features, these are the columns of the dataset (except the result column)
         for feature_index in range(num_features):
             feature_values = dataset[:, feature_index]
+            # all possbile unique values of that feature (column)
             possible_thresholds = np.unique(feature_values)
-            for threshold in possible_thresholds: 
+            # loop over all the feature values present in the data
+            for threshold in possible_thresholds:
+                # get current split based on threshold
                 dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                # check if childs are not null
                 if len(dataset_left)>0 and len(dataset_right)>0:
                     y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    # compute information gain
                     curr_info_gain = self.information_gain(y, left_y, right_y, "gini")
+                    # update the best split if needed
                     if curr_info_gain>max_info_gain:
                         best_split["feature_index"] = feature_index
                         best_split["threshold"] = threshold
@@ -71,14 +77,17 @@ class DecisionTreeClassifier():
                         best_split["info_gain"] = curr_info_gain
                         max_info_gain = curr_info_gain
                         
+        # return best split
         return best_split
     
     def split(self, dataset, feature_index, threshold):
+        # it splits the dataset rows based on the given threshold
         dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
         dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
         return dataset_left, dataset_right
     
     def information_gain(self, parent, l_child, r_child, mode="entropy"):
+        # entropy or gini index determines how much information gain there is
         weight_l = len(l_child) / len(parent)
         weight_r = len(r_child) / len(parent)
         if mode=="gini":
@@ -102,14 +111,16 @@ class DecisionTreeClassifier():
             p_cls = len(y[y == cls]) / len(y)
             gini += p_cls**2
         return 1 - gini
-        
+
+
+
     def calculate_leaf_value(self, Y):
         
         Y = list(Y)
         return max(Y, key=Y.count)
     
     def print_tree(self, tree=None, indent=" "):
-        
+        # visualize tree
         if not tree:
             tree = self.root
 
@@ -142,20 +153,37 @@ class DecisionTreeClassifier():
             return self.make_prediction(x, tree.left)
         else:
             return self.make_prediction(x, tree.right)
+"""
+3.  A feladatot a HAZI06.py-ban old meg.
+    Az órán megírt DecisionTreeClassifier-t fit-eld fel az első feladatban lementett csv-re. 
+    A feladat célja az, hogy határozzuk meg azt, hogy a vonatok késnek-e vagy sem. 0p <= x < 5p --> nem késik, ha 5 < x --> késik.
+    Az adatoknak a 20% legyen test és a splitelés random_state-je pedig 41 (mint órán)
+    A testset-en 80% kell elérni. Ha megvan a minimum százalék, akkor azzal paraméterezd fel a decisiontree-t és azt kell leadni.
 
-col_name = ['sepal_lenght', 'sepal_width', 'petal_length', 'petal_width', 'type']
-data = pd.read_csv("data/Iris.csv",skiprows=1, header=None, names=col_name)
+    A leadásnál csak egy fit kell, ezt azzal a paraméterre paraméterezd fel, amivel a legjobb accuracy-t elérted.
 
+    A helyes paraméter megtalálásához használhatsz grid_search-öt.
+    https://www.w3schools.com/python/python_ml_grid_search.asp 
+
+4.  A tanításodat foglald össze 4-5 mondatban a HAZI06.py-ban a fájl legalján kommentben. Írd le a nehézségeket, mivel próbálkoztál, mi vált be és mi nem. Ezen kívül írd le 10 fitelésed eredményét is, hogy milyen paraméterekkel probáltad és milyen accuracy-t értél el. 
+Ha ezt feladatot hiányzik, akkor nem fogadjuk el a házit!
+"""
+
+col_name = ['stop_sequence','from_id,to_id','status,line','type','day','part_of_the_day','delay']
+data = pd.read_csv("C:\\Users\\Akos\\Documents\\BEVADAT2022232\\HAZI\\HAZI06\\NJ.csv",skiprows=1, header=None, names=col_name)
+#data = pd.read_csv("NJ.csv",skiprows=1, header=None, names=col_name)
 X = data.iloc[:, :-1].values
 Y = data.iloc[:, -1].values.reshape(-1,1)
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=.2, random_state=41)
 
+depths=[1,2,3,4,5,6]
+scores=[]
+for depth in depths:
+    classifier = DecisionTreeClassifier(min_samples_split=depth, max_depth=depth)
+    classifier.fit(X_train, Y_train)
 
-
-classifier = DecisionTreeClassifier(min_samples_split=3, max_depth=3)
-classifier.fit(X_train, Y_train)
-
-Y_pred = classifier.predict(X_test)
-print(accuracy_score(Y_test, Y_pred))
+    Y_pred = classifier.predict(X_test)
+    scores.append(accuracy_score(Y_test, Y_pred))
+print(scores)
 
 
